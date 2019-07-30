b47b77baca132f8f6d4e91b0b7d6a6febd8bf4ab
Heitor <heitorbc3@gmail.com>
2019-07-30 16:22:55 -0300

    Adicionar Style e Html

---
+++ script.js
@@ -0,0 +1,33 @@
+// function mostrarMouse(event){
+//     if(event.pageY >=1 & event.pageX >= 1){
+//     }
+//     else{
+//         confirm("Voce deseja sair desta pagina")
+//     }
+// }
+
+// window.onmousemove = mostrarMouse;
+
+// let texto = document.querySelector("h1");
+
+// localStorage.setItem('nome' , 'Heitor')
+
+// texto.innerHTML= `Saja bem vindo ${localStorage.nome}`
+
+// let caixaTexto = document.querySelector("input");
+// let botao = document.querySelector("button")
+// let texto = document.querySelector("p");
+// let naosou = document.querySelector("a")
+
+// function acessar() {
+//     localStorage.setItem('nome', caixaTexto.value);
+//     texto.innerHTML = `Seja bem vindo ${localStorage.nome}`
+//     naosou.innerHTML = `Não é ${localStorage.nome} ?`
+// }
+
+// function limpar(){
+//     localStorage.clear();
+// }
+
+// botao.onclick = acessar;
+
